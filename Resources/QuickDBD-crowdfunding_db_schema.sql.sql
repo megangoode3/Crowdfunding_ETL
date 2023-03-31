606a7b3dc3eae5a48c548235bee659370fcea949
megangoode3 <megangoode3@gmail.com>
2023-03-30 19:07:49 -0500

    final

--- Resources/QuickDBD-crowdfunding_db_schema.sql.sql
+++ Resources/QuickDBD-crowdfunding_db_schema.sql.sql
@@ -2,6 +2,8 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/6dQRQ0
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+DROP TABLE 
+
 
 CREATE TABLE "contacts" (
     "contact_id" INT(25)   NOT NULL,
